6601e2d35c4a6f687a57964ee93a5154c42f96ba
matiasnarrua <47338063+matiasnarrua@users.noreply.github.com>
2020-10-19 19:34:00 -0300

    Add files via upload

--- Arrua.Matias.ProgIPP1G/main.c
+++ Arrua.Matias.ProgIPP1G/main.c
@@ -1,13 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct{
+int id;
+char nombre[];
+int infectados;
+int recuperados;
+int muertos;
+
+}ePais;
+
+void actualizarRecuperados(ePais pais);
+
 char invertirCadena(char cadena []);
 
 int main () {
+    ePais pais[10];
+    pais.id[1]=1000;
+    pais.nombre[1]="Argentina";
+    pais.infectados=500;
+    pais.recuperados=600;
+    pais.muertos=400;
+
+    int recuperadosDeldia = 5000;
+
     char cadena[10] ={"UTN-FRA"};
 
 	invertirCadena(cadena);
 	printf("%s",cadena);
+
+	actualizarRecuperados(pais, recuperadosDeldia)
+
+
+
 	return 0;
 }
 
@@ -24,3 +49,10 @@ char invertirCadena(char cadena[]) {
   return cadena;
 }
 
+
+
+void actualizarRecuperados(ePais pais, int recuperados){
+
+pais.recuperados += recuperados
+
+}
